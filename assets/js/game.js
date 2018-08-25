06fa5ce76ef47bf808070b8e1ce57d3c556f2a57
wan03 <38962452+wan03@users.noreply.github.com>
2018-08-25 09:07:37 -0400

    Pseudo Coded most of it

---
+++ assets/js/game.js
@@ -0,0 +1,41 @@
+// Declare needed variables
+
+
+
+
+
+
+// Research how to do an object to put logic in it
+
+
+
+
+// Set the way the computer is going to randomly choose words
+
+
+
+// Set the way the user is going to select letters
+
+
+
+
+// Check for whether the user guessed letter matches any of the character in the randomly generated word
+
+
+// If the character matches reveal that letter from the word
+
+
+
+// If the character does not match then reveal one of the parts of the hangman picture
+
+
+// If the word is guess create a win condition at the end
+
+
+
+// if user runs out of chances display lose condition at the end
+
+
+
+
+//Reset game and choose a different word
