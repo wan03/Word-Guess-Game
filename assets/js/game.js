1ee60abac701420492e6b528651f4ecb0b2c1216
wan03 <38962452+wan03@users.noreply.github.com>
2018-08-30 21:10:49 -0400

    Added letter detection and computer randominly picking word

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,64 @@
 // Declare needed variables
 
 
+var wordChoices = [
+  "throw",
+  "kick",
+  "punch",
+  "judo",
+  "karate",
+  "grappling",
+  "choke",
+  "hyperextension",
+  "defense",
+  "tournament",
+  "arm bar"
+
+];
+
+var word
+var lives = 10
+var showlives = document.getElementById("lives");
+var game = {
+  wins: 0,
+  losses: 0,
+  // computerGuess:
+};
+
+var guessedLetters = document.getElementById("guessedLetters");
+
+// Set the way the user is going to select letters, use functions!
+
+var userGuess = function storeKey(event) {
+  return (event.key);
+};
+
+document.onkeyup = function(event) {
+  var keypress = event.key.toLowerCase();
+  var userGuess = storeKey(event);
+
+  
+  if (keypress == "a" || keypress == "b" || keypress == "c" || keypress == "d" || keypress == "f" || keypress == "g" || keypress == "h" || keypress == "j" || keypress == "k" || keypress == "l" || keypress == "m" || keypress == "n" || keypress == "o" || keypress == "p" || keypress == "q" || keypress == "r" || keypress == "s" || keypress == "t" || keypress == "u" || keypress == "v" || keypress == "w" || keypress == "x" || keypress == "y" || keypress == "z") {
+    
+
+  guessedLetters.innerHTML += userGuess
+
+  };
+
+
+
+  // var guessedLetters = document.getElementById("guessedLetters")
+
+  // guessedLetters.textContent = userGuess
+
+};
+
+function storeKey(event) {
+  return ("" + event.key);
+};
+  // console.log(userGuess)
+
+// userGuess = document.onkeyup(event)
 
 
 
@@ -11,11 +69,17 @@
 
 
 // Set the way the computer is going to randomly choose words
+var start = document.getElementById("startGame")
+
+start.addEventListener("click", function(){
+  var wordGuess = wordChoices[Math.floor(Math.random() * Choices.length)];
+
+  console.log(wordGuess);
 
 
 
-// Set the way the user is going to select letters, use functions!
-
+});
+  
 
 
 
